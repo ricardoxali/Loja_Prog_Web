3a73d5f0fa88883de588ba050f62ae5e37e237f4
ricardoxali <x.ricardo@academico.ifrn.edu.br>
2026-05-03 20:44:43 -0300

    Primeiro commit

---
+++ loja/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import *
+
+class FabricanteAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+    exclude = ('msgPromocao',)
+    fields = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria')
+    search_fields = ('Produto',)
+
+admin.site.register(Fabricante, FabricanteAdmin)
+admin.site.register(Categoria)
+admin.site.register(Produto, ProdutoAdmin)
